c2045ff46586dbc8e0e5b725f9c654165423cfff
raymondcastro-hub <raymond.castro@student.northampton.edu>
2025-08-23 21:30:58 -0400

    Prompt for numbers and display sum

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,19 @@
-// YOUR NAME HERE
+// Raymond Castro
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+    int first_int, second_int;
+    
+    // Prompt user for input
+    cout << "Enter first integer: ";
+    cin >> first_int;
 
+    cout << "Enter second integer: ";
+    cin >> second_int;
+
+    // Display result
+    cout << "\nThe sum is " << (first_int + second_int) << ".\n";
+
+    return 0;
 }
